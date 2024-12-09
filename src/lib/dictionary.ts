b2af280163dc3242e9fc74cdbcf4ac01be63fc4e
vampirewy <haveaballmyball@hotmail.com>
2024-12-09 16:52:49 +0800

    fix: modify dictionary logic

--- src/lib/dictionary.ts
+++ src/lib/dictionary.ts
@@ -1,17 +1,15 @@
 import type { Locale } from "@/config/i18n";
+import { glob } from 'glob';
+import path from 'path';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const dictionaries: { [key: string]: () => Promise<any> } = {};
 
-// 动态引入所有语言包
-const requireContext = require.context("@/dictionaries", false, /\.json$/);
+const dictionaryFiles = glob.sync('./src/dictionaries/*.json');
 
-requireContext.keys().forEach((key: string) => {
-  const locale = key.match(/\/(\w+)\.json$/)?.[1];
-  if (locale) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    dictionaries[locale] = () => requireContext(key);
-  }
+dictionaryFiles.forEach((file) => {
+  const locale = path.basename(file, '.json');
+  dictionaries[locale] = () => import(`@/dictionaries/${locale}.json`).then(module => module.default);
 });
 
 export const getDictionary = async (locale: Locale) => {
